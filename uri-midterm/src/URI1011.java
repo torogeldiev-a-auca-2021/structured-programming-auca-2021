53239e4e535d375dfe9e33babf6430eb4967cd53
Torogeldiev-T <tilek2402@gmail.com>
2021-10-17 12:41:20 +0600

    present error

--- uri-midterm/src/URI1011.java
+++ uri-midterm/src/URI1011.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
 public class URI1011 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double r = input.nextDouble();
+        double PI = 3.14159;
+        double V = 4.0 / 3 *PI * r * r * r;
+
+        System.out.printf("Volume = %.3f\n", V);
+
+
+        
+
+
+    }
 }
