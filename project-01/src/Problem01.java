10599490637121b94498ccff59e6a84b336648c2
torogeldiev_a <torogeldiev_a@auca.kg>
2021-11-03 13:33:42 +0600

    .

---
+++ project-01/src/Problem01.java
@@ -0,0 +1,20 @@
+import processing.core.*;
+
+public class Problem01 extends PApplet {
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+    }
+
+    public void draw() {
+        background(0, 0, 0);
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("Problem01");
+    }
+
+}
