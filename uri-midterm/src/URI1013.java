5008c27340105483235d49111e47686f1159d8b6
Torogeldiev-T <tilek2402@gmail.com>
2021-11-09 12:51:42 +0600

    solved

--- uri-midterm/src/URI1013.java
+++ uri-midterm/src/URI1013.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
+
 public class URI1013 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        int great = (a + b + Math.abs(a - b)) / 2;
+        int great2 = (great + c + Math.abs(great - c)) / 2;
+
+        System.out.println(great2 + " eh o maior");
+
 
 
 
