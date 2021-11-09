6f66b2d7bb6a77f8979ac51a0dc5d730f1db6213
Torogeldiev-T <tilek2402@gmail.com>
2021-11-09 13:02:06 +0600

    solved

---
+++ uri-midterm/src/URI1016.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class URI1016 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+
+        int kilom =( x * 2);
+
+        System.out.println(kilom + " minutos");
+
+
+
+    }
+
+
+}
