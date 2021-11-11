0f03b72f3f93d170feea93ec484f89ddd27aff58
Torogeldiev-T <tilek2402@gmail.com>
2021-11-12 01:53:03 +0600

    solved

---
+++ uri-midterm/src/URI1003.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class URI1003 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int A = input.nextInt();
+        int B = input.nextInt();
+
+        int sum = A + B;
+
+        System.out.println("SOMA = "+ sum);
+
+
+
+    }
+}
---
+++ uri-midterm/src/URI1004.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class URI1004 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+        int y = input.nextInt();
+
+        int p = x * y;
+
+        System.out.println("PROD = " + p);
+
+
+    }
+}
