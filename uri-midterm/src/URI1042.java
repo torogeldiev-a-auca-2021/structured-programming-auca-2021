60965b1b5630131a8cad7fe86d10995a544aea12
Torogeldiev-T <tilek2402@gmail.com>
2021-11-13 22:34:31 +0600

    1050 solved

---
+++ uri-midterm/src/URI1042.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class URI1042 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+        int y = input.nextInt();
+        int z = input.nextInt();
+
+
+
+    }
+}
---
+++ uri-midterm/src/URI1050.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class URI1050 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+
+        if (x == 61 ) {
+            System.out.println("Brasilia");
+        }else if (x ==71) {
+            System.out.println("Salvador");
+        }else if (x == 11) {
+            System.out.println("Sao Paulo");
+        }else if (x == 21) {
+            System.out.println("Rio de Janeiro");
+        }else if (x == 32 ) {
+            System.out.println("Juiz de Fora");
+        }else if (x == 19) {
+            System.out.println("Campinas");
+        }else if (x == 27) {
+            System.out.println("Vitoria");
+        }else if (x == 31) {
+            System.out.println("Belo Horizonte");
+        }else  {
+            System.out.println("DDD nao cadastrado");
+
+
+            }
+        }
+
+    }
+
