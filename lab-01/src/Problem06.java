db8e9af1558e85720d28b56f1e2985d2224e661f
Torogeldiev-T <tilek2402@gmail.com>
2021-11-16 00:41:34 +0600

    solved

--- lab-01/src/Problem06.java
+++ lab-01/src/Problem06.java
@@ -1,2 +1,9 @@
 public class Problem06 {
+    public static void main(String[] args) {
+
+        System.out.println("     j       A     V      V       A");
+        System.out.println("     j      A A     V    V       A A");
+        System.out.println("j    j     AAAAA     V  V       AAAAA");
+        System.out.println(" j j      A     A     V        A     A");
+    }
 }
