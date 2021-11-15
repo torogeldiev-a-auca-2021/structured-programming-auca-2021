2de2c3f64cd228c6680f8e4bc7800884fb327d98
Torogeldiev-T <tilek2402@gmail.com>
2021-11-16 00:52:15 +0600

    solved

--- lab-01/src/Problem07.java
+++ lab-01/src/Problem07.java
@@ -1,4 +1,4 @@
-public class Probem07 {
+public class Problem07 {
     public static void main(String[] args) {
         System.out.println("a    a^2    a^3");
         System.out.println("1    1      1");
---
+++ lab-01/src/Problem08.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Problem08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a1 = input.nextInt();
+        int a2 = input.nextInt();
+        int a3 = input.nextInt();
+        int a4 = input.nextInt();
+        int a5 = input.nextInt();
+        int a6 = input.nextInt();
+        int a7 = input.nextInt();
+        int a8 = input.nextInt();
+        int a9 = input.nextInt();
+
+        int sum = a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9;
+        System.out.println(sum);
+
+
+
+    }
+}
