deb8d6e718c617c64db1cf0238dc8ba7b454d6ac
Torogeldiev-T <tilek2402@gmail.com>
2021-11-16 22:07:28 +0600

    solved

--- lab-02/src/Problem06.java
+++ lab-02/src/Problem06.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class Problem06 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a degree in Celsius: ");
+        System.out.print("Enter a degree in Celsius: ");
+        double t = input.nextDouble();
+
+        double f = (9.0 / 5) * t  + 32;
+        System.out.printf("%.0f Celsius is %.1f Fahrenheit",t,f);
 
     }
 }
