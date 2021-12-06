a21db20b4331a66a8650d56aebc7eec06a52e283
Torogeldiev-T <tilek2402@gmail.com>
2021-12-06 23:46:17 +0600

    pr 2 solved

--- project-01/src/Problem02.java
+++ project-01/src/Problem02.java
@@ -12,6 +12,9 @@ public class Problem02 extends PApplet {
     public static float colorGreen;
     public static float colorBlue;
     public static float circleRadius;
+    public static float dX;
+    public static float dY;
+    public static float direction;
 
 
 
@@ -22,12 +25,20 @@ public class Problem02 extends PApplet {
     public void setup() {
         float centerX = width / 2f;
         float centerY = height / 2f;
+        circleRadius = 80;
         circleRPosX = centerX;
         circleRPosY = centerY;
-        circleGPosX = centerX + 10;
+        circleGPosX = centerX + circleRadius;
         circleGPosY = centerY;
-        circleBPosX = centerX + 20;
+        circleBPosX = centerX + circleRadius * 2;
         circleBPosY = centerY;
+        colorRed = 255;
+        colorGreen = 255;
+        colorBlue = 255;
+        dX = 10;
+        dY = 10;
+        direction = -1;
+
 
 
 
@@ -35,6 +46,13 @@ public class Problem02 extends PApplet {
 
     public void draw() {
         background(0, 0, 0);
+        fill(colorRed , 0 ,0);
+        circle(circleRPosX,circleRPosY,circleRadius);
+        fill(0,colorGreen,0);
+        circle(circleGPosX,circleGPosY,circleRadius);
+        fill( 0, 0 ,colorBlue);
+        circle(circleBPosX,circleBPosY,circleRadius);
+
     }
 
     public static void main(String[] args) {
