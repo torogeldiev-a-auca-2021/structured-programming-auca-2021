a0acc2836d3d21f6b68f2552245f140ef2710399
Torogeldiev-T <tilek2402@gmail.com>
2021-12-14 01:03:25 +0600

    45

--- project-01/src/Problem03.java
+++ project-01/src/Problem03.java
@@ -1,8 +1,9 @@
 import processing.core.*;
 
 public class Problem03 extends PApplet {
-    final static float RECT_WIDTH = 400;
-    final static float RECT_HEIGHT = 200;
+    final static float RECT_WIDTH = 600;
+    final static float RECT_HEIGHT = 100;
+
 
     public void settings() {
         fullScreen();
@@ -16,11 +17,20 @@ public class Problem03 extends PApplet {
 
     public void draw() {
         background(0, 0, 0);
-        float rectTopLeftX = width / 2f - RECT_WIDTH / 2f;
-        float rectTopLeftY = height / 4f - RECT_HEIGHT / 2f;
+        float rectTopLeftX1 = width / 2f - RECT_WIDTH / 2f;
+        float rectTopLeftY1 = height / 4f - RECT_HEIGHT / 2f;
+        float rectTopLeftX2 = width / 2f - RECT_WIDTH / 2f;
+        float rectTopLeftY2 = height / 3f - RECT_HEIGHT / 4f;
+        float rectTopLeftX3 = width / 2f - RECT_WIDTH / 2f;
+        float rectTopLeftY3 = height / 2f - RECT_HEIGHT / 1.3f;
+
+
 
         fill (0, 0 , 255);
-        rect(rectTopLeftX , rectTopLeftY, RECT_WIDTH, RECT_HEIGHT);
+        rect(rectTopLeftX1 , rectTopLeftY1, RECT_WIDTH, RECT_HEIGHT);
+        rect(rectTopLeftX2 , rectTopLeftY2, RECT_WIDTH, RECT_HEIGHT);
+        rect(rectTopLeftX3 , rectTopLeftY3, RECT_WIDTH, RECT_HEIGHT);
+
 
 
         fill (0,0,100);
@@ -28,6 +38,11 @@ public class Problem03 extends PApplet {
         fill (255, 255, 0);
         text("mouseX: " + mouseX, width / 2f , height / 2f);
         text("mouseY: " + mouseX, width / 2f , height / 2f + height / 30f);
+
+        if (rectTopLeftX1 <= mouseX && mouseX <= rectTopLeftX1 + RECT_WIDTH && rectTopLeftY1 <= mouseY && mouseY <= rectTopLeftY1 + RECT_HEIGHT){
+            fill(255, 255, 255);
+            text("Year: 1995. PYPL Index: 2", width / 2f , height - height / 10f);
+        }
 
     }
 
