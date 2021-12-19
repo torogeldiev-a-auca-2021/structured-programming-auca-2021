048835ef19d5a3636d43482dc2472793cca56fb5
torogeldiev_a <torogeldiev_a@auca.kg>
2021-12-19 15:00:51 +0600

    1114 solved

---
+++ uri-final/src/URI1114.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class URI1114 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x;
+        do {
+            x = input.nextInt();
+            if (x != 2002)
+                System.out.println("Senha Invalida");
+        } while (x != 2002) ;
+        {
+            System.out.println("Acesso Permitido");
+
+            }
+        }
+        }
+
