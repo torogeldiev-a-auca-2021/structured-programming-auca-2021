83097afd45b6da436e1f4919af27640cd2e127de
torogeldiev_a <torogeldiev_a@auca.kg>
2021-12-21 21:37:34 +0600

    pr 1 solved

---
+++ lab-07/src/Problem01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Problem01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n [] = new int[13];
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++){
+            int die1 = 1 + (int)(Math.random() * 6);
+            int die2 = 1 + (int)(Math.random() * 6);
+            int sum = die1 + die2;
+            n[sum]++;
+        }
+        for (int i = 2; i < 13;i++){
+            System.out.printf("%d: %d\n", i, n[i]);
+        }
+    }
+}
---
+++ lab-07/src/Problem02.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class Problem02 {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (true) {
+            int year = readNumber("Year: ", "Incorrect number of year", 1, 100000);
+
+            int month = readNumber("Month: ", "Incorrect number of month", 1, 12);
+
+            int days = getNumberOfDays(year, month);
+
+            System.out.println("Number of days: " + days);
+        }
+    }
+
+    static int readNumber(String msg, String errorMsg, int beg, int end) {
+        System.out.print(msg);
+        if (!input.hasNextInt()) {
+            System.exit(0);
+        }
+        int result = input.nextInt();
+        if (result < beg || end < result) {
+            System.out.println(errorMsg + result);
+            System.exit(1);
+        }
+        return result;
+    }
+
+    static int getNumberOfDays(int year, int month) {
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int result = days[month - 1];
+        if (isLeapYear(year) && month == 2) {
+            ++result;
+        }
+        return result;
+    }
+
+
+
+    static boolean isLeapYear(int year) {
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+    }
+}
