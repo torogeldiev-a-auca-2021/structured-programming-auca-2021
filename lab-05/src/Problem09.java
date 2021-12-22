97179934ea17dd182cb9669d7e0ebf7628b42046
Torogeldiev-T <tilek2402@gmail.com>
2021-12-22 13:07:24 +0600

    1180 solved

--- lab-05/src/Problem09.java
+++ lab-05/src/Problem09.java
@@ -1,8 +1,30 @@
 import java.util.Scanner;
 
 public class Problem09 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter the string: ");
+        String s = inp.nextLine();
+
+        int low = 0;
+        int high = s.length() - 1;
+
+        boolean isPalindrome = true;
+        while (low < high) {
+            if (s.charAt(low) != s.charAt(high)) {
+                isPalindrome = false;
+                break;
+            }
+            low++;
+            high--;
+        }
+        if (isPalindrome){
+            System.out.println(s + " is a palindrome");
+        } else {
+            System.out.println(s + " is not a palindrome");
+        }
     }
 }
